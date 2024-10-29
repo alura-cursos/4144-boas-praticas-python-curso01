aa359450a129f849ab0a250a3f18b65b0058f0ce
Laís Urano <lais.urano@gmail.com>
2024-10-29 15:25:29 -0300

    black formatter

--- app/main.py
+++ app/main.py
@@ -3,22 +3,8 @@ from fastapi import FastAPI
 from app.routers import routers_produtos, routers_usuarios
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 MENSAGEM_HOME: str = "Bem-vindo à API de Recomendação de Produtos"
 
-'''comentário'''
-"""comentario"""
 
 # Criando o App
 app = FastAPI()
@@ -28,9 +14,8 @@ app.include_router(routers_produtos.router)
 
 # Iniciando o servidor
 
+
 @app.get("/")
 def home() -> Dict[str, str]:
     global MENSAGEM_HOME
     return {"mensagem": MENSAGEM_HOME}
-
-
--- app/routers/routers_usuarios.py
+++ app/routers/routers_usuarios.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter
 from typing import List
+from fastapi import APIRouter
 from app.models.models_usuarios import Usuario
 
 router = APIRouter()
 
 
-usuarios: List[Usuario]                 =[]
+usuarios: List[Usuario] = []
 
-contador_usuario: int          =1
+contador_usuario: int = 1
 # Rota para cadastrar usuários
+
 
 @router.post("/usuarios/", response_model=Usuario)
 def criar_usuario(nome: str) -> Usuario:
@@ -18,7 +19,9 @@ def criar_usuario(nome: str) -> Usuario:
     contador_usuario += 1
     return novo_usuario
 
+
 # Rota para listar usuários
+
 
 @router.get("/usuarios/", response_model=List[Usuario])
 def listar_usuarios() -> List[Usuario]:
